c6c8118c8cd83ebfe68b6c5af3a1a278fcadf51b
John Faria <johnwbf@hotmail.com>
2020-11-24 19:36:59 -0300

    ➕ feat: add express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Test')
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({ msg: 'ok' })
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
