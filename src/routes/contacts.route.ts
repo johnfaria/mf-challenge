be3e0e05cd9f7099fc34f06683dbe0d0761d7422
John Faria <johnwbf@hotmail.com>
2020-11-25 18:13:20 -0300

    🐛 fix: fix contact route name macapa

--- src/routes/contacts.route.ts
+++ src/routes/contacts.route.ts
@@ -8,9 +8,9 @@ import { Router } from 'express'
 import { tokenValidation } from '../middleware/auth.middleware'
 const router = Router()
 
-router.post('/amapa', tokenValidation, postMacapa)
+router.post('/macapa', tokenValidation, postMacapa)
 router.post('/varejao', tokenValidation, postVarejao)
-router.get('/amapa', tokenValidation, getMacapa)
+router.get('/macapa', tokenValidation, getMacapa)
 router.get('/varejao', tokenValidation, getVarejao)
 
 export default router
